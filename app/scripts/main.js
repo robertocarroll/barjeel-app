026d54ff25cf22af3a8b114dc7ad571daf2e152c
robertocarroll <robertocarroll@gmail.com>
2015-01-16 21:09:22 +0000

    Initial commit. Removed Twitter Bootstrap and added Ratchet.

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global Barjeel, $*/
+
+
+window.Barjeel = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    Barjeel.init();
+});
