2415c4f0cb38ebe496566ff7b081d5ffd770f3fe
robertocarroll <robertocarroll@gmail.com>
2015-09-30 12:20:47 +0100

    Added offline message to modal images.

--- app/scripts/views/play-modal-one.js
+++ app/scripts/views/play-modal-one.js
@@ -7,7 +7,24 @@ Barjeel.Views = Barjeel.Views || {};
 
     Barjeel.Views.PlayModalOne = Backbone.Modal.extend({
         template: JST['app/scripts/templates/play-modal-one.hbs'],
-        submitEl: '.modal-icon'
+        submitEl: '.modal-icon',
+        onShow: function() {
+        	var $image = $('.large-image');
+        	var $offline = $('#offline-message');
+
+        	$offline.show();
+
+			    $image.on('load', function () {
+			      $offline.hide();
+			      console.log('Hiding offline message');
+			    });
+
+			    if ($image[0].complete) {
+			      $image.load();
+			    }
+
+					console.log('modal window 2 showing now');
+				}
     });
 
 })();
