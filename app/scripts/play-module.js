96b32f4e1e6cda1abb06092bf3a2ef99ab7018f5
robertocarroll <rob@wearethoughtfox.com>
2015-12-02 15:11:38 +0000

    Refactored question logic so it checks for no questions left before getting another question. Still need to create a template to handle that situation.

--- app/scripts/play-module.js
+++ app/scripts/play-module.js
@@ -1,105 +1,146 @@
 BarjeelApp.module("PlayModule", function(PlayModule, BarjeelApp, Backbone, Marionette, $, _) {
 
-	PlayModule.startPlay = function() {
-		var gameState = JSON.parse(localStorage.getItem('barjeel-app-game-state'));
-      if (gameState == null || gameState.gameInProgress === false) {
-        BarjeelApp.PlayModule.startQuiz();
-      } else {
-        BarjeelApp.PlayModule.resumeQuiz();
-      }
+PlayModule.startPlay = function() {
+  // Feature detect + local reference
+  function lsTest(){
+    var test = 'test';
+    try {
+        localStorage.setItem(test, test);
+        localStorage.removeItem(test);
+        return true;
+    } catch(e) {
+        return false;
+    }
+  }
+
+  if(lsTest() === true){
+    var gameState = JSON.parse(localStorage.getItem('barjeel-app-game-state'));
+    if (gameState == null || gameState.gameInProgress === false) {
+      PlayModule.startQuiz();
+    }
+    else {
+      PlayModule.resumeQuiz();
+    }
+  }
+
+  else{
+    console.log("no local storage");
+     // BarjeelApp.PlayModule.playQuizNoLocal();
+  }
 }
 
-	PlayModule.startQuiz = function() {
-		console.log("play quiz started");
-		PlayModule.newCollection = new Barjeel.Collections.PlayCollection();
-		PlayModule.newCollection.fetch({
-			success: function(MyCollectionAllQuestions, response, options) {
-				console.log("fetch questions success");
-				//add data to local storage
-				localStorage.setItem('barjeel-app-questions', JSON.stringify(PlayModule.newCollection.toJSON()));
-				PlayModule.mainLayout = new Barjeel.Views.PlayMainView();
-				PlayModule.gameState = new Barjeel.Models.PlayGameStateModel();
-				//set game in progress to true and add it to localstorage
-				PlayModule.gameState.set({
-					gameInProgress: true
-				});
-				localStorage.setItem('barjeel-app-game-state', JSON.stringify(PlayModule.gameState.toJSON()));
-				BarjeelApp.PlayModule.getQuestion();
-			}
-		});
-	}
+PlayModule.startQuiz = function() {
+  console.log("play quiz started");
+  PlayModule.newCollection = new Barjeel.Collections.PlayCollection();
+  PlayModule.newCollection.fetch({
+    success: function(MyCollectionAllQuestions, response, options) {
+      console.log("fetch questions success");
+      //add data to local storage
+      localStorage.setItem('barjeel-app-questions', JSON.stringify(PlayModule.newCollection.toJSON()));
+      PlayModule.mainLayout = new Barjeel.Views.PlayMainView();
+      PlayModule.gameState = new Barjeel.Models.PlayGameStateModel();
+      //set game in progress to true and add it to localstorage
+      PlayModule.gameState.set({
+        gameInProgress: true
+      });
+      localStorage.setItem('barjeel-app-game-state', JSON.stringify(PlayModule.gameState.toJSON()));
+      PlayModule.getQuestion();
+    }
+  });
+}
 
-	PlayModule.resumeQuiz = function() {
-		console.log("Restarting quiz");
-		PlayModule.mainLayout = new Barjeel.Views.PlayMainView();
-		PlayModule.gameState = new Barjeel.Models.PlayGameStateModel();
-		BarjeelApp.PlayModule.getQuestion();
-	}
+PlayModule.resumeQuiz = function() {
+  console.log("Restarting quiz");
+  PlayModule.mainLayout = new Barjeel.Views.PlayMainView();
+  PlayModule.gameState = new Barjeel.Models.PlayGameStateModel();
 
-	PlayModule.getQuestion = function() {
+  //get questions from localstorage
+  PlayModule.newLocalCollection = new Barjeel.Collections.PlayCollection(JSON.parse(localStorage.getItem('barjeel-app-questions')));
 
-		//get questions from localstorage
-		var newLocalCollection = new Barjeel.Collections.PlayCollection(JSON.parse(localStorage.getItem('barjeel-app-questions')));
+  PlayModule.questions = PlayModule.newLocalCollection
+    .chain()
+    .filter(function(m) {
+      return m.get('use')
+    })
+    .pluck('id')
+    .value();
 
-		//get the IDs of all questions which haven't been used
-		var questions = newLocalCollection.chain()
-			.filter(function(m) {
-				return m.get('use')
-			})
-			.pluck('id')
-			.value();
-
-			console.log(questions);
-
-      var counterTemp = BarjeelApp.PlayModule.gameState.get("questionCounter");
-      counterTemp += 1;
-      BarjeelApp.PlayModule.gameState.set(("questionCounter"), counterTemp);
-
-		if (questions.length > 0) {
-			// get random ID from question ID array
-			var rand = questions[_.random(questions.length - 1)];
-			console.log('chosen ID value: ' + rand);
-
-			//get a model from a collection, specified by ID
-			var currentQuestion = newLocalCollection.get(rand);
-			console.log(currentQuestion);
-
-			//set the status of that question to used
-			currentQuestion.set("use", false);
-			localStorage.setItem('barjeel-app-questions', JSON.stringify(newLocalCollection.toJSON()));
-
-			//show the main layout
-			BarjeelApp.allRegion.showAnimated(BarjeelApp.PlayModule.mainLayout, {
-				animationType: 'slideRight'
-			});
-
-			BarjeelApp.PlayModule.mainLayout.questionRegion.show(new Barjeel.Views.PlayQuestionView({
-				model: currentQuestion
-			}));
-
-			//get the lives and show them in the lives region
-			var livesCount = PlayModule.gameState;
-			BarjeelApp.PlayModule.mainLayout.livesRegion.show(new Barjeel.Views.PlayLivesView({
-				model: livesCount
-			}));
-			BarjeelApp.PlayModule.mainLayout.livesRegion.$el.show();
-		}
-
-		//if there's no more questions
-    else {
-        console.log('No more questions');
-        var livesCount =  BarjeelApp.PlayModule.gameState;
-        //show the main layout
-				BarjeelApp.allRegion.showAnimated(BarjeelApp.PlayModule.mainLayout, {
-					animationType: 'slideRight'
-				});
-
-				BarjeelApp.PlayModule.mainLayout.questionRegion.show(new Barjeel.Views.PlayWinGameOverView({
-					model: livesCount
-				}));
-				BarjeelApp.PlayModule.mainLayout.livesRegion.$el.hide();
+    if (PlayModule.questions.length > 0) {
+     console.log('this is how many questions from resumeQuiz: ' + PlayModule.questions);
+     PlayModule.getQuestion();
     }
 
-	}
+    //if there's no more questions
+  else {
+      console.log('No more questions');
+  }
+}
+
+PlayModule.getQuestion = function() {
+
+// Get all the questions from the collection and filter by use value
+  PlayModule.questions = PlayModule.newLocalCollection
+  .chain()
+  .filter(function(m) {
+    return m.get('use')
+  })
+  .pluck('id')
+  .value();
+
+  if (PlayModule.questions.length > 0) {
+    var questions = PlayModule.questions;
+    console.log('this is how many questions: ' + questions);
+
+    // get random ID from question ID array
+    var rand = questions[_.random(questions.length - 1)];
+    console.log('chosen ID value: ' + rand);
+
+    //get a model from a collection, specified by ID
+    var currentQuestion = PlayModule.newLocalCollection.get(rand);
+    console.log(currentQuestion);
+
+    //set the status of that question to used
+    currentQuestion.set("use", false);
+    localStorage.setItem('barjeel-app-questions', JSON.stringify(PlayModule.newLocalCollection.toJSON()));
+
+    //increase the question count by one
+    var counterTemp = PlayModule.gameState.get("questionCounter");
+    counterTemp += 1;
+    PlayModule.gameState.set(("questionCounter"), counterTemp);
+
+    //show the main layout
+    BarjeelApp.allRegion.showAnimated(BarjeelApp.PlayModule.mainLayout, {
+      animationType: 'slideRight'
+    });
+
+    //show the current question in the question region
+    BarjeelApp.PlayModule.mainLayout.questionRegion.show(new Barjeel.Views.PlayQuestionView({
+      model: currentQuestion
+    }));
+
+    //get the lives and show them in the lives region
+    var livesCount = PlayModule.gameState;
+    BarjeelApp.PlayModule.mainLayout.livesRegion.show(new Barjeel.Views.PlayLivesView({
+      model: livesCount
+    }));
+    BarjeelApp.PlayModule.mainLayout.livesRegion.$el.show();
+  }
+
+  //if there's no more questions
+  else {
+    console.log('No more questions');
+    var livesCount =  PlayModule.gameState;
+    //show the main layout
+    BarjeelApp.allRegion.showAnimated(BarjeelApp.PlayModule.mainLayout, {
+      animationType: 'slideRight'
+    });
+
+    BarjeelApp.PlayModule.mainLayout.questionRegion.show(new Barjeel.Views.PlayWinGameOverView({
+      model: livesCount
+    }));
+    BarjeelApp.PlayModule.mainLayout.livesRegion.$el.hide();
+  }
+}
 
 });
+
--- app/scripts/views/play-question-view.js
+++ app/scripts/views/play-question-view.js
@@ -3,82 +3,86 @@
 Barjeel.Views = Barjeel.Views || {};
 
 (function() {
-	'use strict';
+  'use strict';
 
-	Barjeel.Views.PlayQuestionView = Backbone.Marionette.ItemView.extend({
+  Barjeel.Views.PlayQuestionView = Backbone.Marionette.ItemView.extend({
 
-		initialize: function() {
-			console.log("Question view initialised: " + JSON.stringify(BarjeelApp.PlayModule.gameState));
-		},
+    initialize: function() {
+      console.log("Question view initialised: " + JSON.stringify(BarjeelApp.PlayModule.gameState));
+    },
 
-		template: JST['app/scripts/templates/play-question-view.hbs'],
+    template: JST['app/scripts/templates/play-question-view.hbs'],
 
-		onDomRefresh: function() {
-			$('.content').animate({
-				scrollTop: 0
-			}, 100);
-			console.log('scrolled content');
-		},
+    onDomRefresh: function() {
+      $('.content').animate({
+        scrollTop: 0
+      }, 100);
+      console.log('scrolled content');
+    },
 
-		events: {
-			"tap .questionWrapper": "checkAnswer"
-		},
+    events: {
+      "tap .questionWrapper": "checkAnswer"
+    },
 
-		getSelected: function() {
-			return this.model.get("selected");
-		},
+    getSelected: function() {
+      return this.model.get("selected");
+    },
 
-		select: function(value) {
-			this.model.set("selected", value);
-		},
+    select: function(value) {
+      this.model.set("selected", value);
+    },
 
-		checkAnswer: function(e) {
-			$('.questionWrapper').not(active).removeClass('active');
-			var active = $(e.currentTarget);
-			active.addClass('active');
-			var answer = active.data('answer');
-			this.select(parseInt(answer, 10));
-			var currentSelection = this.model.get("selected"),
-				correctAnswer = this.model.get("correctAnswer");
+    checkAnswer: function(e) {
+      $('.questionWrapper').not(active).removeClass('active');
+      var active = $(e.currentTarget);
+      active.addClass('active');
+      var answer = active.data('answer');
+      this.select(parseInt(answer, 10));
+      var currentSelection = this.model.get("selected"),
+      correctAnswer = this.model.get("correctAnswer");
 
-				console.log(currentSelection);
-				console.log(correctAnswer);
+      console.log(currentSelection);
+      console.log(correctAnswer);
 
-			if (currentSelection === correctAnswer) {
-				var counterTemp = BarjeelApp.PlayModule.gameState.get("correctCounter");
-				counterTemp += 1;
-				BarjeelApp.PlayModule.gameState.set(("correctCounter"), counterTemp);
-				this.model.set("correct", true);
-				var currentQuestion = this.model;
-				BarjeelApp.PlayModule.mainLayout.questionRegion.show(new Barjeel.Views.PlayRightView({
-					model: currentQuestion
-				}));
-			} else if (currentSelection !== correctAnswer) {
-				var livesTemp = BarjeelApp.PlayModule.gameState.get("gamelives");
-				livesTemp -= 1;
-				BarjeelApp.PlayModule.gameState.set(("gamelives"), livesTemp);
-				this.model.set("correct", false);
-				this.checkLives();
-			}
-		},
+      if (currentSelection === correctAnswer) {
+        var counterTemp = BarjeelApp.PlayModule.gameState.get("correctCounter");
+        counterTemp += 1;
+        BarjeelApp.PlayModule.gameState.set(("correctCounter"), counterTemp);
+        this.model.set("correct", true);
+        var currentQuestion = this.model;
+        BarjeelApp.PlayModule.mainLayout.questionRegion.show(new Barjeel.Views.PlayRightView({
+          model: currentQuestion
+        }));
+      }
 
-		checkLives: function() {
-			var livesTemp = BarjeelApp.PlayModule.gameState.get("gamelives");
+      else if (currentSelection !== correctAnswer) {
+        var livesTemp = BarjeelApp.PlayModule.gameState.get("gamelives");
+        livesTemp -= 1;
+        BarjeelApp.PlayModule.gameState.set(("gamelives"), livesTemp);
+        this.model.set("correct", false);
+        this.checkLives();
+      }
+    },
 
-			if (livesTemp > 0) {
-				BarjeelApp.PlayModule.mainLayout.questionRegion.$el.hide();
-				BarjeelApp.PlayModule.mainLayout.wrongRegion.show(new Barjeel.Views.PlayWrongView());
-				BarjeelApp.PlayModule.mainLayout.wrongRegion.$el.show();
-			} else {
-				var livesCount = BarjeelApp.PlayModule.gameState;
-				BarjeelApp.PlayModule.mainLayout.questionRegion.show(new Barjeel.Views.PlayGameOverView({
-					model: livesCount
-				}));
+    checkLives: function() {
+      var livesTemp = BarjeelApp.PlayModule.gameState.get("gamelives");
 
-				BarjeelApp.PlayModule.mainLayout.livesRegion.$el.hide();
-			}
-		}
+      if (livesTemp > 0) {
+        BarjeelApp.PlayModule.mainLayout.questionRegion.$el.hide();
+        BarjeelApp.PlayModule.mainLayout.wrongRegion.show(new Barjeel.Views.PlayWrongView());
+        BarjeelApp.PlayModule.mainLayout.wrongRegion.$el.show();
+      }
 
-	});
+      else {
+        var livesCount = BarjeelApp.PlayModule.gameState;
+        BarjeelApp.PlayModule.mainLayout.questionRegion.show(new Barjeel.Views.PlayGameOverView({
+          model: livesCount
+        }));
+
+        BarjeelApp.PlayModule.mainLayout.livesRegion.$el.hide();
+      }
+    }
+
+  });
 
 })();
