9e9b7f7d980676206f3da21a552f5837bdb9b9cb
robertocarroll <rob@wearethoughtfox.com>
2015-12-18 13:27:21 +0000

    Increased the hit area on the top bar home icon and the back arrow.

--- app/scripts/play-module.js
+++ app/scripts/play-module.js
@@ -14,41 +14,39 @@ PlayModule.startPlay = function() {
 }
 
 PlayModule.getQuestion = function() {
-
   var tempQuestions = PlayModule.newCollection;
-
   var questions = tempQuestions.chain()
   .pluck('id')
   .value();
 
-    // get random ID from question ID array
-    var rand = questions[_.random(questions.length - 1)];
-    console.log('chosen ID value: ' + rand);
+  // get random ID from question ID array
+  var rand = questions[_.random(questions.length - 1)];
+  console.log('chosen ID value: ' + rand);
 
-    //get a model from a collection, specified by ID
-    var currentQuestion = tempQuestions.get(rand);
-    console.log(currentQuestion);
+  //get a model from a collection, specified by ID
+  var currentQuestion = tempQuestions.get(rand);
+  console.log(currentQuestion);
 
-    //increase the question count by one
-    var counterTemp = PlayModule.gameState.get("questionCounter");
-    counterTemp += 1;
-    PlayModule.gameState.set(("questionCounter"), counterTemp);
+  //increase the question count by one
+  var counterTemp = PlayModule.gameState.get("questionCounter");
+  counterTemp += 1;
+  PlayModule.gameState.set(("questionCounter"), counterTemp);
 
-    //show the main layout
-    BarjeelApp.allRegion.showAnimated(BarjeelApp.PlayModule.mainLayout, {
-      animationType: 'slideRight'
-    });
+  //show the main layout
+  BarjeelApp.allRegion.showAnimated(BarjeelApp.PlayModule.mainLayout, {
+    animationType: 'slideRight'
+  });
 
-    //show the current question in the question region
-    BarjeelApp.PlayModule.mainLayout.questionRegion.show(new Barjeel.Views.PlayQuestionView({
-      model: currentQuestion
-    }));
+  //show the current question in the question region
+  BarjeelApp.PlayModule.mainLayout.questionRegion.show(new Barjeel.Views.PlayQuestionView({
+    model: currentQuestion
+  }));
 
-    //get the lives and show them in the lives region
-    var livesCount = PlayModule.gameState;
-    BarjeelApp.PlayModule.mainLayout.livesRegion.show(new Barjeel.Views.PlayLivesView({
-      model: livesCount
-    }));
-    BarjeelApp.PlayModule.mainLayout.livesRegion.$el.show();
+  //get the lives and show them in the lives region
+  var livesCount = PlayModule.gameState;
+  BarjeelApp.PlayModule.mainLayout.livesRegion.show(new Barjeel.Views.PlayLivesView({
+    model: livesCount
+  }));
+  BarjeelApp.PlayModule.mainLayout.livesRegion.$el.show();
   }
 });
